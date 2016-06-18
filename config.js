66c23fc3c84a00db298e079317d4a44ba46cbf48
Ben Barnett <ben@univthink.com>
2016-06-18 10:50:16 -0400

    TEST

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  database: process.env.MONGOLAB_URI || 'localhost/SpotRoom',
+  database: process.env.MONGOLAB_URI || 'localhost:5000/SpotRoom',
   spotify_client_id: 'ef34a2841479421ea9a4851986c94b7d' ,
   spotify_client_secret: 'a788bb3831b04560a5a6971d5bc2fade',
   spotify_callback_url: 'https://sporti.herokuapp.com/auth/callback/'
